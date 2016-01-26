f106d1322b74b3c6ae87e38d6bf0ace7ef8ec06e
unknown <s0042767@LSTLTROB1.rgare.net>
2016-01-25 18:02:35 -0600

    Final Commit from RGA

--- homework.R
+++ homework.R
@@ -9,6 +9,11 @@ library(ggplot2)
 
 ggplot(x, aes(x=SPHEQ, y=MYOPIC)) + geom_point() + stat_smooth(method="glm", family="binomial", se=FALSE)
 
+#Home1.2
 
-
+icu<-read.csv(file="./data/icu.csv")
+str(icu)
+class(icu)
+ggplot(icu, aes(x=AGE, y=STA)) + geom_point() + stat_smooth(method="glm", family="binomial", se=FALSE)
+curve(predict(icu, data.frame(AGE=x), type="response"), add=TRUE)
 
